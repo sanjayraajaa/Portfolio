16d9877644898f1ea71aa1499c5bc781c68cec74
Sanjay Raja <thesanjayraja@gmail.com>
2025-02-21 00:23:18 +0530

    classes updated for Social Icons

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -2,20 +2,21 @@ import instagram from '../assets/instagram.svg';
 import github from '../assets/github.svg';
 import linkedin from '../assets/linkedin.svg';
 import mail from '../assets/mail.svg';
+import '../style/Home.css';
 
 const SocialIcons = () => {
     return (
-        <div className="social_icons flex gap-4 mt-4">
-            <a href="https://instagram.com/yourprofile" target="_blank" rel="noopener noreferrer">
+        <div className='soc_containder'>
+            <a href="https://instagram.com/sanjayraajaa" target="_blank" rel="noopener noreferrer" className='soc_link'>
                 <img src={instagram} alt="Instagram" className="w-8 h-8 hover:opacity-80 transition-all" />
             </a>
-            <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/sanjayraajaa" target="_blank" rel="noopener noreferrer" className='soc_link'>
                 <img src={github} alt="GitHub" className="w-8 h-8 hover:opacity-80 transition-all" />
             </a>
-            <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+            <a href="https://linkedin.com/in/sanjayraajaa" target="_blank" rel="noopener noreferrer" className='soc_link'>
                 <img src={linkedin} alt="LinkedIn" className="w-8 h-8 hover:opacity-80 transition-all" />
             </a>
-            <a href="mailto:your@email.com">
+            <a href="mailto:thesanjayraja@gmail.com" className='soc_link'>
                 <img src={mail} alt="Email" className="w-8 h-8 hover:opacity-80 transition-all" />
             </a>
         </div>
