767642433aac2b842bc9584f3b883b76631ea69e
Sanjay Raja <thesanjayraja@gmail.com>
2025-03-31 00:20:35 +0530

    Assests Structured

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import profile from "../assets/profile.jpg"; 
+import profile from "../assets/personal/profile.jpg"; 
 import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope, FaUserCircle } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import Typewriter from 'typewriter-effect';
